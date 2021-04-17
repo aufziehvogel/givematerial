379da80b912db536d461b70d75decb4a5d694f98
Stefan Koch <programming@stefan-koch.name>
2021-04-17 14:13:00 +0200

    recommend five lyrics, targeting about 5 new words

--- croatian/list_texts.py
+++ croatian/list_texts.py
@@ -3,6 +3,19 @@ import collections
 import json
 from pathlib import Path
 import re
+import heapq
+import dataclasses
+import uuid
+
+
+@dataclasses.dataclass
+class TextStats:
+    artist: str
+    title: str
+    text: str
+    unknown: int
+    learning: int
+    total: int
 
 
 def load_lemma_frequencies(freqs_file: Path):
@@ -45,6 +58,9 @@ if __name__ == '__main__':
     artist_words = collections.defaultdict(collections.Counter)
 
     print('{artist};{title};{len(relevant_lemmas)};{len(learning_from_text)};{len(known_from_text)};{len(unknown_from_text)};{min(all_word_freqs)};{sum(all_word_freqs) / float(len(all_word_freqs))};{max(all_word_freqs)};{min(unknown_word_freqs)};{sum(unknown_word_freqs) / float(len(unknown_word_freqs))};{max(unknown_word_freqs)}')
+
+    recommendations = []
+
     for artist, title, text in iterate_lyrics(lyrics_folder):
         cache_file = cache_folder / f'{artist}-{title}.json'
         if cache_file.is_file():
@@ -89,6 +105,17 @@ if __name__ == '__main__':
         for word in unknown_from_text:
             unknown_word_freqs.append(lemmas[word])
 
+        # add a uuid so that heapq never tries to compare TextStats to TextStats
+        order = (abs(len(unknown_from_text) - 5), -len(learning_from_text), -len(relevant_lemmas), uuid.uuid4())
+        text_stats = TextStats(
+            artist=artist,
+            title=title,
+            text=text,
+            unknown=len(unknown_from_text),
+            learning=len(learning_from_text),
+            total=len(relevant_lemmas))
+        heapq.heappush(recommendations, (order, text_stats))
+
         #print(f'Different words: {len(relevant_lemmas)}')
         #print(f'Learning words: {len(learning_from_text)}')
         #print(f'Known words: {len(known_from_text)}')
@@ -97,24 +124,31 @@ if __name__ == '__main__':
         #print(f'Unknown word frequencies: [{min(unknown_word_freqs)}, {sum(unknown_word_freqs) / float(len(unknown_word_freqs))}, {max(unknown_word_freqs)}]')
         #print(unknown_from_text)
 
-        print(f'{artist};{title};{len(relevant_lemmas)};{len(learning_from_text)};{len(known_from_text)};{len(unknown_from_text)};{min(all_word_freqs)};{sum(all_word_freqs) / float(len(all_word_freqs))};{max(all_word_freqs)};{min(unknown_word_freqs)};{sum(unknown_word_freqs) / float(len(unknown_word_freqs))};{max(unknown_word_freqs)}')
+        #print(f'{artist};{title};{len(relevant_lemmas)};{len(learning_from_text)};{len(known_from_text)};{len(unknown_from_text)};{min(all_word_freqs)};{sum(all_word_freqs) / float(len(all_word_freqs))};{max(all_word_freqs)};{min(unknown_word_freqs)};{sum(unknown_word_freqs) / float(len(unknown_word_freqs))};{max(unknown_word_freqs)}')
 
         artist_summary[artist].append(len(relevant_lemmas))
         artist_words[artist].update(relevant_lemmas)
 
-    for artist, lengths in artist_summary.items():
-        print(artist)
-        print(f'Min length: {min(lengths)}')
-        print(f'Max length: {max(lengths)}')
-        print(f'Avg length: {sum(lengths) / float(len(lengths))}')
+    for _, ts in heapq.nsmallest(5, recommendations):
+        print(f'== {ts.artist} - {ts.title} (learning {ts.learning}, total {ts.total}, unknown {ts.unknown}) ==')
+        print()
+        print(ts.text)
+        print()
+        print()
 
-    for artist, words in artist_words.items():
-        known_from_artist = set(words).intersection(known_words)
-        unknown_from_artist = collections.Counter(words)
-        for word in known_words:
-            del unknown_from_artist[word]
+    #for artist, lengths in artist_summary.items():
+    #    print(artist)
+    #    print(f'Min length: {min(lengths)}')
+    #    print(f'Max length: {max(lengths)}')
+    #    print(f'Avg length: {sum(lengths) / float(len(lengths))}')
 
-        print(artist)
-        print(f'Total vocab: {len(words)}')
-        print(f'Known vocab: {len(known_from_artist)}')
-        print(f'Unknown words: {unknown_from_artist.most_common()[0:50]}')
+    #for artist, words in artist_words.items():
+    #    known_from_artist = set(words).intersection(known_words)
+    #    unknown_from_artist = collections.Counter(words)
+    #    for word in known_words:
+    #        del unknown_from_artist[word]
+
+    #    print(artist)
+    #    print(f'Total vocab: {len(words)}')
+    #    print(f'Known vocab: {len(known_from_artist)}')
+    #    print(f'Unknown words: {unknown_from_artist.most_common()[0:50]}')
