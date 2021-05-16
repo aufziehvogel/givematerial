35575b926abf0c9e0c53205034911f8a85eaddb2
Stefan Koch <programming@stefan-koch.name>
2021-05-16 14:53:49 +0200

    use actual easy version for text analysis

--- japanese/retrieval/nhk_easy.py
+++ japanese/retrieval/nhk_easy.py
@@ -4,7 +4,7 @@ import json
 from pathlib import Path
 import requests
 import time
-from typing import List
+from typing import List, Tuple
 import xextract
 
 
@@ -14,26 +14,26 @@ class Text:
     text: str
 
 
-def fetch_texts_urls(main_url: str) -> List[str]:
+def fetch_texts_urls(main_url: str) -> List[Tuple[str, str]]:
     r = requests.get(main_url)
     data = json.loads(r.content.decode('utf-8-sig'))[0]
 
     urls = []
     for date in data:
         for item in data[date]:
-            urls.append(item['news_web_url'])
+            url = f'https://www3.nhk.or.jp/news/easy/{item["news_id"]}/{item["news_id"]}.html'
+            tup = (url, item['title'])
+            urls.append(tup)
     return urls
 
 
-def fetch_text(text_url: str) -> Text:
+def fetch_text(text_url: str, title: str) -> Text:
     r = requests.get(text_url)
     html = r.content.decode('utf-8-sig')
     sections = xextract.String(
-        css='.module--detail-content', attr='_all_text').parse(html)
+        css='.article-main__body', attr='_all_text').parse(html)
     text = '\n\n'.join([section.strip() for section in sections])
     text = text.replace('\t', '')
-
-    title = xextract.String(css='h1.content--title span').parse(html)[0]
 
     return Text(title=title, text=text)
 
@@ -51,9 +51,9 @@ if __name__ == '__main__':
     main_page = 'https://www3.nhk.or.jp/news/easy/news-list.json'
     texts_urls = fetch_texts_urls(main_page)
 
-    for text_url in texts_urls:
+    for text_url, title in texts_urls:
         filename = text_url.split('/')[-1]
-        text = fetch_text(text_url)
+        text = fetch_text(text_url, title)
 
         print(f'Writing text for {filename}')
         with open(outdir / f'{filename}.json', 'wt') as f:
@@ -62,6 +62,7 @@ if __name__ == '__main__':
                 'title': f'{text.title}',
                 'text': text.text,
                 'language': 'jp',
+                'url': text_url,
             }
             json.dump(data, f)
 
