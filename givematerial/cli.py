fb684c19943ed0de52d21c722feab447083b44c3
Stefan Koch <programming@stefan-koch.name>
2021-05-24 14:47:20 +0200

    add command to prepare cache

--- givematerial/cli.py
+++ givematerial/cli.py
@@ -9,18 +9,31 @@ def main():
     logging.basicConfig(level=os.getenv('LOGLEVEL'))
 
     parser = argparse.ArgumentParser(description='Get text recommendations')
-    parser.add_argument('--language', '-l', dest='language', required=True)
+    subparsers = parser.add_subparsers(dest='subparser_name')
+
+    prepare_parser = subparsers.add_parser('prepare-text-cache')
+    prepare_parser.add_argument(
+        '--language', '-l', dest='language', required=True)
+
+    recommend_parser = subparsers.add_parser('recommend')
+    recommend_parser.add_argument(
+        '--language', '-l', dest='language', required=True)
     args = parser.parse_args()
 
-    for ts in recommendation.calc_recommendations(args.language):
-        print(
-            f'== {ts.title} (learning {len(ts.learning)}, total {ts.total}, unknown {len(ts.unknown)}) ==')
-        print()
-        print(' '.join(ts.unknown))
-        print()
-        print(ts.text)
-        print()
-        print()
+    if args.subparser_name == 'prepare-text-cache':
+        # TODO: Call better functions for preparing the cache, not rely on
+        # calc_recommendations() creating the cache
+        recommendation.calc_recommendations(args.language)
+    elif args.subparser_name == 'recommend':
+        for ts in recommendation.calc_recommendations(args.language):
+            print(
+                f'== {ts.title} (learning {len(ts.learning)}, total {ts.total}, unknown {len(ts.unknown)}) ==')
+            print()
+            print(' '.join(ts.unknown))
+            print()
+            print(ts.text)
+            print()
+            print()
 
 
 def manage_read():
